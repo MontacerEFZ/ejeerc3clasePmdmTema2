8593f7ed6c8163a1dd228c92533d5e679242044e
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-12-11 09:57:28 +0100

    good

--- app/src/main/java/montacer/elfazazi/ejeerc3clasepmdmtema2/MainActivity.java
+++ app/src/main/java/montacer/elfazazi/ejeerc3clasepmdmtema2/MainActivity.java
@@ -3,12 +3,46 @@ package montacer.elfazazi.ejeerc3clasepmdmtema2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import montacer.elfazazi.ejeerc3clasepmdmtema2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnInsertarMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.btnConsultarMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.btnModificarMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.btnEliminarMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
