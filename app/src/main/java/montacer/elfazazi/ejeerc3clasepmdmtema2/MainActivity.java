5c6c10993631c57d10f20a5966fd386b3ccc37ce
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-12-11 10:13:15 +0100

    good

--- app/src/main/java/montacer/elfazazi/ejeerc3clasepmdmtema2/MainActivity.java
+++ app/src/main/java/montacer/elfazazi/ejeerc3clasepmdmtema2/MainActivity.java
@@ -4,12 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.stmt.query.In;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import montacer.elfazazi.ejeerc3clasepmdmtema2.Configuraciones.Configuracion;
 import montacer.elfazazi.ejeerc3clasepmdmtema2.databinding.ActivityMainBinding;
+import montacer.elfazazi.ejeerc3clasepmdmtema2.helpers.AlumnosHelper;
+import montacer.elfazazi.ejeerc3clasepmdmtema2.modelos.Alumno;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private ArrayList<Alumno> listaAlumnos;
+    private AlumnosHelper helper;
+    private Dao<Alumno, Integer> daoAlumno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +30,22 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        listaAlumnos = new ArrayList<>();
+        helper = new AlumnosHelper(this, Configuracion.BD_NAME, null, Configuracion.BD_VERSION);
+
+        if (helper != null){
+            daoAlumno = helper.getDaoAlumnos();
+            try {
+                listaAlumnos.addAll(daoAlumno.queryForAll());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
         binding.btnInsertarMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                insertarAlumno();
             }
         });
 
@@ -44,5 +69,35 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void insertarAlumno() {
+        if (binding.txtNombreMain.getText().toString().isEmpty() || binding.txtApellidosMain.getText().toString().isEmpty() ||
+                binding.txtNota1Main.getText().toString().isEmpty() || binding.txtNota2Main.getText().toString().isEmpty() ||
+                binding.txtNota3Main.getText().toString().isEmpty()){
+            Toast.makeText(this, "faltan datos", Toast.LENGTH_SHORT).show();
+        }else{
+            Alumno a = new Alumno();
+            a.setNombre(binding.txtNombreMain.getText().toString());
+            a.setApellidos(binding.txtApellidosMain.getText().toString());
+            a.setNota1(Float.parseFloat(binding.txtNota1Main.getText().toString()));
+            a.setNota2(Float.parseFloat(binding.txtNota2Main.getText().toString()));
+            a.setNota3(Float.parseFloat(binding.txtNota3Main.getText().toString()));
+
+            try {
+                daoAlumno.create(a);
+                a.setId(daoAlumno.extractId(a));
+                listaAlumnos.add(a);
+
+                binding.txtNombreMain.setText("");
+                binding.txtApellidosMain.setText("");
+                binding.txtNota1Main.setText("");
+                binding.txtNota2Main.setText("");
+                binding.txtNota3Main.setText("");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 }
